4fe6821eb3f8e155d49c8a1c41ee1c3fe6619393
harshsanghavi1 <harshsanghavi099@gmail.com>
2025-02-22 15:25:02 +0530

    Create aes__master.m

---
+++ aes__master.m
@@ -0,0 +1,20 @@
+clc
+clear all
+close all
+% Global Variable Declarations
+preallocations;
+
+% Initial input plaintext and key
+plaintext = '0123456789ABCDEF';
+% plaintext = input('Type in an input message (16 characters or less):\n','s');
+plaintext = zerofill(plaintext);
+key = 'MachinIntllignce';
+% key = input('Type in a secret key/password (16 characters or less):\n','s');
+key = zerofill(key);
+
+% Key Schedule
+round_keys = key_schedule(double(key));
+% Message Encryption
+ciphertext = aes_encryption(plaintext,round_keys);
+% Message Decryption
+plaintext_recov = aes_decryption(ciphertext, round_keys);
